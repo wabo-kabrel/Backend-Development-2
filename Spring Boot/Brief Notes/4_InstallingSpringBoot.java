bc74270ed3a5e8b89cadd7330d509bcccad36277
Kabrel <wabokabrel@gmail.com>
2025-08-08 15:27:43 +0100

    Updates

---
+++ Spring Boot/Brief Notes/4_InstallingSpringBoot.java
@@ -0,0 +1,23 @@
+// There are 03 ways to instal Spring boot:
+//1. Using Initializr (Recommended for Beginners)
+// Website https://start.spring.io
+// - Chpoose Maven, Java, Spring Boot version.
+// - Add dependencies like Spring Web, Spring Data JPA, etc.
+// - Add dependencies like Spring Web, Spring Data JPA, etc.
+
+
+//2. Using Spring Boot CLI
+// - Install Spring Boot CLI:
+/*
+brew install springboot   # macOS
+sdk install springboot    # Linux via SDKMAN
+choco install springboot  # Windows via Chocolatey
+*/ 
+// - Check version `spring --version`
+// - Run a quick app wth CLI `spring run app.groovy`
+
+
+//3.  Using Your IDE
+// Many IDEs let you create a Spring Boot project directly:
+// - IntelliJ → File → New → Spring Initializr Project.
+// - Eclipse with Spring Tools → File → New → Spring Starter Project.
---
+++ Spring Boot/Brief Notes/5_SpringBootStarters.java
@@ -0,0 +1,25 @@
+//1. What is a Starter?
+// A starter in Spring Boot is a pre-package set of dependencies that
+// you can add to your project to enable a specific feature without manually
+// searching for and configuring each library yourself.
+
+// Think of it as a combo meal:
+// - Instead of ordering bread, meat, cheese, and sauce separately, you just 
+//   order a "Burger Meal" and it comes with everything in the right quantity.
+// - In coding terms: Instead of adding Tomcat, Jackson, and Spring MVC one-by-one,
+//   you just add spring-boot-starter-web.
+
+
+//2. Why Starters Exist
+// Before starters:
+// - You had to find compatible versions of libraries.
+// - If one library version didn’t work with another, you were in dependency hell.
+// - You needed to write a lot of boilerplate configuration.
+
+// After starters:
+// - You just add one dependency.
+// - Spring Boot pulls in all required libraries and versions that work well together.
+// - Auto-configuration kicks in to set everything up.
+
+
+//  Popular Starters (From the Docs)
