93062f09454bf06c50688683b96050f567644eee
Kabrel <wabokabrel@gmail.com>
2025-08-08 14:35:07 +0100

    Updates

---
+++ Spring Boot/Brief Notes/3_SystemRequirements.java
@@ -0,0 +1,29 @@
+//1. Java Verson
+// - Spring Boot runs on Java, so you must have a supported Java version installed.
+// - Spring Boot always supports the latest Java LTS (Long-Term Support) version and
+// - sometimes the most recent non-LTS.
+// - Check your Java version using `java -version`
+
+
+//2. Build Tools
+// You can build Spring Boot apps using:
+// - Maven (default choice in most tutorials)
+// - Gradle (faster builds, more modern syntax)
+
+//  Recommendation for beginners: Start with Maven because:
+// - Most Spring Boot docs & examples use Maven.
+// - Easier to read XML-based pom.xml files when starting out.
+// Check you Maven version using `mvn -v`
+// if you don't have it, download it at  https://maven.apache.org/download.cgi
+
+//3. IDE (Integrated Development Environment)
+// You can use any Java IDE, but:
+// - IntelliJ IDEA (Community Edition is free) → Best Spring Boot integration.
+// - Eclipse → Also good with Spring Tools.
+// - VS Code with Java extensions → Lightweight alternative.
+
+//4. Common Mistakes to Avoid
+// - Using Java 8 or 11 for the latest Spring Boot — some features won’t work.
+// - Forgetting to set JAVA_HOME environment variable.
+// - Mixing Maven & Gradle in the same project (pick one).
+
